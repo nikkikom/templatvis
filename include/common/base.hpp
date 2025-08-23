f16b0f5585162582bd1c70aff530f505039d2833
Nikki Chumakov <nchumakov@nvidia.com>
2025-08-23 20:58:57 +0200

    fix windows base class

--- include/common/base.hpp
+++ include/common/base.hpp
@@ -1,2 +1,13 @@
 #pragma once
-namespace common { struct Base { virtual ~Base() = default; }; }
+#include "common/export.hpp"
+
+namespace common {
+struct COMMON_API Base {
+  virtual ~Base()
+#if defined(COMMON_INLINE_DTOR)
+    = default;   // inline
+#else
+    ;            // out-of-line
+#endif
+};
+} // namespace common
